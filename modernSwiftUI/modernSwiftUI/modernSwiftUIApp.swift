bb0d0c7ca7f97f32e0bfcdca6325535cb4474d22
Charles <1070726094@qq.com>
2021-07-21 16:04:04 +0800

    project init

---
+++ modernSwiftUI/modernSwiftUI/modernSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  modernSwiftUIApp.swift
+//  modernSwiftUI
+//
+//  Created by charles on 2021/7/21.
+//
+
+import SwiftUI
+
+@main
+struct modernSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
