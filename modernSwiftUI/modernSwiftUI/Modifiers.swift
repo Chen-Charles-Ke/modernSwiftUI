85f1c3b2c7303c620e16a5527966a5b8cf0b5917
Charles <1070726094@qq.com>
2021-08-09 23:36:43 +0800

    add sans

---
+++ modernSwiftUI/modernSwiftUI/Modifiers.swift
@@ -0,0 +1,33 @@
+//
+//  Modifiers.swift
+//  modernSwiftUI
+//
+//  Created by charles on 2021/8/9.
+//
+import SwiftUI
+
+struct ShadowModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
+            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+    }
+}
+
+struct FontModifier: ViewModifier {
+    var style: Font.TextStyle = .body
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(style, design: .default))
+    }
+}
+
+struct CustomFontModifier: ViewModifier {
+    var size: CGFloat = 28
+    
+    func body(content: Content) -> some View {
+        content.font(.custom("WorkSans-Bold", size: size))
+    }
+}
+
